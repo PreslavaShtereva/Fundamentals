cbb8c98bf21a984f7fe111bf15dd50a4710e4758
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-03-29 11:18:32 +0200

    Create 01. Order Two Numbers.cpp

---
+++ Basic Syntax - Exercises/01. Order Two Numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	cin >> a >> b;
+
+	if (a > b) {
+		cout << b << " " << a;
+	}
+	else {
+		cout << a << " " << b;
+	}
+
+return 0;
+}
