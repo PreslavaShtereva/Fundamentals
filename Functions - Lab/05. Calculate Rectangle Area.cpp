2a5a4f3941e4e8b483ec2412230cecc81aedbfa1
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-04-01 11:54:43 +0200

    Create 05. Calculate Rectangle Area.cpp

---
+++ Functions - Lab/05. Calculate Rectangle Area.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include<climits>
+using namespace std;
+
+void input(int a, int b) {
+  
+	cout << a * b;
+  
+}
+
+int main() {
+
+	int a, b;
+	cin >> a >> b;
+  
+	input(a, b);
+		
+return 0;
+}
