d6f9287cc2b65cc718090a5fb50dee073749e0dc
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-05-24 23:14:50 +0200

    Create 01. Unused.cpp

---
+++ C++ Fundamentals - Exam Preparation/01. Unused.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<climits>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	string input;
+	getline(cin, input);
+
+	string strUnusedLetters = "abcdefghijklmnopqrstuvwxyz";
+
+	for (size_t idx = 0; idx < input.size(); idx++)
+	{
+		size_t location = strUnusedLetters.find(input[idx]);
+		
+		if (location != string::npos)
+			strUnusedLetters.erase(location, 1);
+	}
+
+	cout << strUnusedLetters << endl;
+
+	return 0;
+}
